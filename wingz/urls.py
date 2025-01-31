f7ffa0e2c4a3d1b7fb0d143cd6bd2c942cb1de83
Lorie Castillano <clorieanne@gmail.com>
2025-01-31 13:43:59 +0800

    Added RideEvents and user authentication API endpoints.

--- wingz/urls.py
+++ wingz/urls.py
@@ -2,9 +2,12 @@ from django.urls import include, path
 from rest_framework import routers
 from core import views as core_views
 
+
 router = routers.DefaultRouter()
 router.register(r'rides', core_views.RideViewSet, basename="rides")
+router.register(r'ride_events', core_views.RideEventViewSet, basename="ride_events")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('rest-auth/', include('dj_rest_auth.urls')),
 ]
